24ebeeebe38d7ab5d492d9c1f383b77b508b828c
eduardo ciudad figueredo <eduardociudadfigueredo@gmail.com>
2026-04-18 16:47:41 -0300

    removi o / da url acho que esta dando bug

--- src/main/java/com/eduardo/biblioteca/config/CorsConfig.java
+++ src/main/java/com/eduardo/biblioteca/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://eduardo-ciudad.github.io/frontend-biblioteca-api/")
+                .allowedOrigins("https://eduardo-ciudad.github.io")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
