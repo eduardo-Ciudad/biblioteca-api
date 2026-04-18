c9a72bfe43fe6bf3503a24074b2147462f91745e
eduardo ciudad figueredo <eduardociudadfigueredo@gmail.com>
2026-04-18 16:40:16 -0300

    mudei o link no cros

--- src/main/java/com/eduardo/biblioteca/config/CorsConfig.java
+++ src/main/java/com/eduardo/biblioteca/config/CorsConfig.java
@@ -10,7 +10,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+                .allowedOrigins("https://eduardo-ciudad.github.io/frontend-biblioteca-api/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
     }
+
 }
